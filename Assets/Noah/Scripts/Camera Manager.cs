ac0a7179055db1678ff4481d01984fc63ab56c99
Linkerpink <noahvanuunen2007@gmail.com>
2024-09-11 15:28:17 +0200

    2 cams working now!!!

--- Assets/Noah/Scripts/Camera Manager.cs
+++ Assets/Noah/Scripts/Camera Manager.cs
@@ -1,18 +1,75 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using System.Collections;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class CameraManager : MonoBehaviour
 {
     private bool enableCameras = false;
     private bool canEnableCameras = true;
 
-    [SerializeField] private Image camMonitorButton; //The button for making the cams appear
+    [SerializeField] private Button camMonitorButton;
+    [SerializeField] private GameObject cameraSystem;
+
+    //Camera Render Textures
+    [SerializeField] private GameObject cam1ART;
+    [SerializeField] private GameObject cam1BRT;
+
+    private enum Cameras
+    {
+        cam1A,
+        cam1B,
+    }
+
+    private Cameras currentCamera;
+
     private void Update()
     {
+        
 
+        switch (currentCamera)
+        {
+            case Cameras.cam1A:
+                cam1ART.SetActive(true);
+                cam1BRT.SetActive(false);
+                break;
+
+            case Cameras.cam1B:
+                cam1ART.SetActive(false);
+                cam1BRT.SetActive(true);
+                break;
+
+            default:
+                break;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)) 
+        {
+            ChangeEnableCameras();
+        }
+
+        if (enableCameras)
+        {
+            cameraSystem.SetActive(true);
+        }
+        else
+        {
+            cameraSystem.SetActive(false);
+        }
+    }
+
+    public void ChangeEnableCameras()
+    {
+        enableCameras = !enableCameras;
+    }
+
+    public void ChangeCam1A()
+    {
+        currentCamera = Cameras.cam1A;
+    }
+
+    public void ChangeCam1B() 
+    {
+        currentCamera = Cameras.cam1B;
     }
 }
