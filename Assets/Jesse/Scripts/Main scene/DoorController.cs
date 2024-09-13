1477d48abc6925d0682ac53ccbbcfc37a99c9524
Jesse3215 <jessefaassen20073215@gmail.com>
2024-09-13 11:32:33 +0200

    Door en lights

---
+++ Assets/Jesse/Scripts/Main scene/DoorController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField] private Door Door;
+
+    private void OnMouseDown()
+    {
+        Door.isOpen = !Door.isOpen;
+    }
+}
