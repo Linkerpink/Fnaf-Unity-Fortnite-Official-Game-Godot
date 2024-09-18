437bf53d4831e1ab0127a81c26d749c7e92130bb
Linkerpink <noahvanuunen2007@gmail.com>
2024-09-18 13:44:48 +0200

    foxy running animation fix sorta

--- Assets/Noah/Scripts/Camera Manager.cs
+++ Assets/Noah/Scripts/Camera Manager.cs
@@ -18,6 +18,9 @@ public class CameraManager : MonoBehaviour
 
     private Animator camChangeStaticAnimator;
 
+    [SerializeField] private GameObject camMonitor;
+    [SerializeField] private float camMonitorAnimationSpeed = 0.05f;
+
     private void Awake()
     {
         camChangeStaticAnimator = GameObject.Find("Cam Change Static").GetComponent<Animator>();
@@ -43,10 +46,24 @@ public class CameraManager : MonoBehaviour
         if (enableCameras)
         {
             cameraSystem.SetActive(true);
+
+            //Animations
+            rawImage.transform.localScale = Vector3.Slerp(rawImage.transform.localScale, new Vector3(1, 1, 1), 00.1f);
+            rawImage.transform.localPosition = Vector3.Slerp(rawImage.transform.localPosition, new Vector3(0, 0, 0), 00.1f);
+
+            camMonitor.transform.localScale = Vector3.Slerp(camMonitor.transform.localScale, new Vector3(1, 1, 1), 00.1f);
+            camMonitor.transform.localPosition = Vector3.Slerp(camMonitor.transform.localPosition, new Vector3(0, 0, 0), 00.1f);
         }
         else
         {
             cameraSystem.SetActive(false);
+
+            //Animations
+            rawImage.transform.localScale = Vector3.Slerp(rawImage.transform.localScale, new Vector3(1, 0, 1), 00.1f);
+            rawImage.transform.localPosition = Vector3.Slerp(rawImage.transform.localPosition, new Vector3(0, -600, 0), 00.1f);
+
+            camMonitor.transform.localScale = Vector3.Slerp(camMonitor.transform.localScale, new Vector3(1, 0, 1), 00.1f);
+            camMonitor.transform.localPosition = Vector3.Slerp(camMonitor.transform.localPosition, new Vector3(0, -600, 0), 00.1f);
         }
     }
 
@@ -66,5 +83,11 @@ public class CameraManager : MonoBehaviour
 
         allCameras[cam].SetActive(true);
         rawImage.texture = cameraArray[cam];
+    }
+
+    private IEnumerator camDisableDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        cameraSystem.SetActive(false);
     }
 }
